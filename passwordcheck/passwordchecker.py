193996a52d9b291dab15181476960f341b3ef5b3
Blaxkeyes <121274600+kidbrando21@users.noreply.github.com>
2023-11-18 19:36:11 -0800

    Update passwordchecker.py

--- passwordcheck/passwordchecker.py
+++ passwordcheck/passwordchecker.py
@@ -1,22 +1,36 @@
 import re
 
-password = input("inset pass here ")
+print("A good password has 12 or more characters, upper and lower case letters, numbers, and symbols Also No words in the english dictionary")
 
-desired_length = 7
+password = input("insert pass here: ")
+
+desired_length = 12
 
 length = len(password)
 
-def check_string(password):
-    has_letters = (re.search(r'[a-zA-z]', password))
-    has_numbers = (re.search(r'\d', password))
-    has_symbols = (re.search(r'[^a-zA-Z0-9\s]', password))
-    
-    if has_letters and has_numbers and has_symbols and length >= desired_length:
-        return ("you have a strong password")
+def check_string(password, wordlist_filename):
+    has_letters = bool(re.search(r'[a-zA-Z]', password))
+    has_lowletters = bool(re.search(r'[a-z]', password))
+    has_numbers = bool(re.search(r'\d', password))
+    has_symbols = bool(re.search(r'[^a-zA-Z0-9\s]', password))
+
+    with open(wordlist_filename, "r") as wordlist_file:
+        wordlist = wordlist_file.read().splitlines()
+
+    if has_letters and has_numbers and has_lowletters and has_symbols and length >= desired_length:
+        contains_words = False
+        for word in wordlist:
+            if word.lower() in password.lower():
+                contains_words = True
+                break
+
+        if contains_words:
+            return "Password is not strong enough."
+        else:
+            return "Strong password."
     else:
-        return ("the password is not strong enough")
+        return "Password does not meet the criteria."
 
-results = check_string(password)
-
+results = check_string(password, "words.txt")
 print(results)
 
